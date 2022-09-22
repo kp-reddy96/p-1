76a1c3a39bd4326616e7617b95430822afa553c2
kp-reddy96 <107455170+kp-reddy96@users.noreply.github.com>
2022-09-22 16:21:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAXW6CH6AYZGHV53V6"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 
 }
